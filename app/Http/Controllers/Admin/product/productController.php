6a49cb4ad13bcec7cba79a216af1f45e740cea26
arif <arifmehrab11@gmail.com>
2020-06-29 16:24:43 -0700

    product Updated Successfullly With Multi Images

--- app/Http/Controllers/Admin/product/productController.php
+++ app/Http/Controllers/Admin/product/productController.php
@@ -146,59 +146,102 @@ class productController extends Controller
     public function update(Request $request, $id)
     {
          // Product Update...
-        //  $productEdit                   = product::find($id);
-        //  $productEdit->category_id      = $request->category;
-        //  $productEdit->subcategory_id   = $request->subcategory;
-        //  $productEdit->brand_id         = $request->brand;
-        //  $productEdit->product_name     = $request->product_name;
-        //  $productEdit->product_code     = $request->product_code;
-        //  $productEdit->product_quantity = $request->product_quantity;
-        //  $productEdit->product_details  = $request->product_details;
-        //  $productEdit->product_color    = $request->product_color;
-        //  $productEdit->product_size     = $request->product_size;
-        //  $productEdit->selling_price    = $request->selling_price;
-        //  $productEdit->discount_price   = $request->discount_price;
-        //  $productEdit->video_link       = $request->video_link;
-        //  $productEdit->main_slider      = $request->main_slider;
-        //  $productEdit->hot_deal         = $request->hot_deal;
-        //  $productEdit->best_rated       = $request->best_rated;
-        //  $productEdit->mid_slider       = $request->mid_slider;
-        //  $productEdit->hot_new          = $request->hot_new;
-        //  $productEdit->trendy           = $request->trendy;
+         $productEdit                   = product::find($id);
+         $productEdit->category_id      = $request->category;
+         $productEdit->subcategory_id   = $request->subcategory;
+         $productEdit->brand_id         = $request->brand;
+         $productEdit->product_name     = $request->product_name;
+         $productEdit->product_code     = $request->product_code;
+         $productEdit->product_quantity = $request->product_quantity;
+         $productEdit->product_details  = $request->product_details;
+         $productEdit->product_color    = $request->product_color;
+         $productEdit->product_size     = $request->product_size;
+         $productEdit->selling_price    = $request->selling_price;
+         $productEdit->discount_price   = $request->discount_price;
+         $productEdit->video_link       = $request->video_link;
+         $productEdit->main_slider      = $request->main_slider;
+         $productEdit->hot_deal         = $request->hot_deal;
+         $productEdit->best_rated       = $request->best_rated;
+         $productEdit->mid_slider       = $request->mid_slider;
+         $productEdit->hot_new          = $request->hot_new;
+         $productEdit->trendy           = $request->trendy;
          // Image One Update
          $image_one = $request->file('image1');
          $image_two = $request->file('image2');
          $image_three = $request->file('image3');
-          // Three image Update 
-        if($image_one && $image_two && $image_three) {
-            dd('image one and image tow and image three');
-        }
-         // Two image Update 
+
+         // Image One And Two Update 
         if($image_one && $image_two) {
-            dd('image one and two');
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_one));
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_two));
+             // Update image One.. 
+             $image_one_name = hexdec(uniqid()).'.'.$image_one->getClientOriginalExtension();
+             Image::make($image_one)->resize(230, 300)->save('Backend/assets/images/product/'. $image_one_name);
+             $productEdit->image_one = $image_one_name;
+             // Update image Two.. 
+             $image_two_name = hexdec(uniqid()).'.'.$image_two->getClientOriginalExtension();
+             Image::make($image_two)->resize(230, 300)->save('Backend/assets/images/product/'. $image_two_name);
+             $productEdit->image_two = $image_two_name;
         }
-         // Two image Update 
+         // Image Two And Three Update 
          if($image_two && $image_three) {
-            dd('image two and thdree');
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_two));
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_three));
+             // Update image One.. 
+             $image_two_name = hexdec(uniqid()).'.'.$image_two->getClientOriginalExtension();
+             Image::make($image_two)->resize(230, 300)->save('Backend/assets/images/product/'. $image_two_name);
+             $productEdit->image_two = $image_two_name;
+             // Update image Three.. 
+           $image_three_name = hexdec(uniqid()).'.'.$image_three->getClientOriginalExtension();
+           Image::make($image_three)->resize(230, 300)->save('Backend/assets/images/product/'. $image_three_name);
+           $productEdit->image_three = $image_three_name;
         }
-         // Two image Update 
-         if($image_one && $image_three) {
-            dd('image one and image three');
+        // Image One And Three Update 
+        if($image_one && $image_three) {
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_one));
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_three));
+            // Update image One.. 
+            $image_one_name = hexdec(uniqid()).'.'.$image_one->getClientOriginalExtension();
+            Image::make($image_one)->resize(230, 300)->save('Backend/assets/images/product/'. $image_one_name);
+            $productEdit->image_one = $image_one_name;
+             // Update image Three.. 
+           $image_three_name = hexdec(uniqid()).'.'.$image_three->getClientOriginalExtension();
+           Image::make($image_three)->resize(230, 300)->save('Backend/assets/images/product/'. $image_three_name);
+           $productEdit->image_three = $image_three_name;
         }
         // One image update
          if(isset($image_one)) {
-             dd('image one');
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_one));
+             // Update image One.. 
+             $image_one_name = hexdec(uniqid()).'.'.$image_one->getClientOriginalExtension();
+             Image::make($image_one)->resize(230, 300)->save('Backend/assets/images/product/'. $image_one_name);
+             $productEdit->image_one = $image_one_name;
          }
          // One image update 
          if(isset($image_two)) {
-             dd('image two');
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_two));
+             // Update image Two.. 
+             $image_two_name = hexdec(uniqid()).'.'.$image_two->getClientOriginalExtension();
+             Image::make($image_two)->resize(230, 300)->save('Backend/assets/images/product/'. $image_two_name);
+             $productEdit->image_two = $image_two_name;
          }
          // One image update
          if(isset($image_three)) {
-            dd('image three');
+            @unlink(public_path('/Backend/assets/images/product/'.$productEdit->image_three));
+            // Update image Three.. 
+           $image_three_name = hexdec(uniqid()).'.'.$image_three->getClientOriginalExtension();
+           Image::make($image_three)->resize(230, 300)->save('Backend/assets/images/product/'. $image_three_name);
+           $productEdit->image_three = $image_three_name;
         }
-        
-         
+        // Save Product Edit Data... 
+        $productEdit->save();
+          // Notification...
+          $notification = array(
+            'message'    => 'Product Updated successfuly',
+            'alert-type' => 'success',
+        );
+        // Redirect
+        return redirect()->route('admin.product.index')->with($notification);
 
     }
 
